3ea5a22de9b647ebfb415f2d75725d1d60fe0acc
Thiago Lagden <lagden@gmail.com>
2019-03-26 17:01:19 -0300

    Emoji
    - Syntax update
    - Added more bytes to work with emojis
    - Updated test and added emoji test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 function toChars(str) {
-	var i=0, arr=[];
+	const arr = [];
+	let i = 0;
 	while (i < str.length) {
 		arr.push(str.charCodeAt(i++));
 	}
@@ -7,20 +8,23 @@ function toChars(str) {
 }
 
 function reduce(hasher, base, value) {
-	var i=0, j, tmp, str='';
-	var chars = base ? value.match(/.{1,2}/g) : toChars(value);
+	let i = 0;
+	let str = '';
+	const chars = base ? value.match(/.{1,5}/ug) : toChars(value);
 	while (i < chars.length) {
-		tmp = base ? parseInt(chars[i++], 16) : chars[i++];
-		for (j=0; j < hasher.length;) tmp ^= hasher[j++];
-		str += base ? String.fromCharCode(tmp) : ('0' + tmp.toString(16)).substr(-2);
+		let tmp = base ? parseInt(chars[i++], 16) : chars[i++];
+		for (let j=0; j < hasher.length;) {
+			tmp ^= hasher[j++];
+		}
+		str += base ? String.fromCharCode(tmp) : tmp.toString(16).padStart(5, '0');
 	}
 	return str;
 }
 
 export function encrypt(key) {
-	return reduce.bind(reduce, toChars(key), 0);
+	return value => reduce(toChars(key), 0, value);
 }
 
 export function decrypt(key) {
-	return reduce.bind(reduce, toChars(key), 1);
+	return value => reduce(toChars(key), 1, value);
 }
--- test/index.js
+++ test/index.js
@@ -21,12 +21,12 @@ test('encrypt()', t => {
 	t.is(typeof val2, 'string', '~> factory produces a string');
 
 	t.isNot(val1, val2, 'hashed-values are not the same');
-	t.is(val1, '7f727b7b78', '~> 1st value is correct');
-	t.is(val2, '6078657b73', '~> 2nd value is correct');
+	t.is(val1, '0007f000720007b0007b00078', '~> 1st value is correct');
+	t.is(val2, '0006000078000650007b00073', '~> 2nd value is correct');
 
 	let val3 = lib.encrypt('hello')('hello');
 	t.isNot(val1, val3, 'result is different w/ new key');
-	t.is(val3, '0a070e0e0d', '~> 3rd value is correct');
+	t.is(val3, '0000a000070000e0000e0000d', '~> 3rd value is correct');
 
 	t.end();
 });
@@ -37,18 +37,31 @@ test('decrypt()', t => {
 	let foo = lib.decrypt(key);
 	t.is(typeof foo, 'function', 'returns a new function');
 
-	let val1 = foo('7f727b7b78');
+	let val1 = foo('0007f000720007b0007b00078');
 	t.is(typeof val1, 'string', '~> factory produces a string');
 
-	let val2 = foo('6078657b73');
+	let val2 = foo('0006000078000650007b00073');
 	t.is(typeof val2, 'string', '~> factory produces a string');
 
 	t.isNot(val1, val2, 'real-values are not the same');
 	t.is(val1, 'hello', '~> 1st value is correct');
 	t.is(val2, 'world', '~> 2nd value is correct');
 
-	let val3 = lib.decrypt('hello')('0a070e0e0d');
+	let val3 = lib.decrypt('hello')('0000a000070000e0000e0000d');
 	t.is(val3, 'hello', '~> 3rd value is correct');
 
 	t.end();
 });
+
+test('emoji', t => {
+	let key = 'foobar';
+	let val = 'Hello 🌔';
+
+	let encryptVal = lib.encrypt(key)(val);
+	let decryptVal = lib.decrypt(key)(encryptVal);
+
+	t.is(encryptVal, '0005f000720007b0007b00078000370d82b0df03', '~> 1st value is correct');
+	t.is(decryptVal, val, '~> 2nd value is correct');
+
+	t.end();
+})
